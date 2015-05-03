8e5c9507f263434027590ddd4e9b1043dc841265
liuyuqi <liuyuqi.crh@gmail.com>
2015-05-03 16:57:31 -0400

    packet sniffing is now stable. Pending to add in many more functionalities...

--- watchdog.py
+++ watchdog.py
@@ -6,14 +6,15 @@ import struct
 
 class WatchdogInfo:
     'basic information of watchdog'
-    watchdog_ipaddr = "" # in the form of string
+    local_ipaddr = "" # in the form of string
     known_ip_addr_dict = {} # a dictionary containing all known ip addresses
     def __init__(self):
         print "\nWelcome to SNS watchdog! System initializing..."
         # gethostbyname(gethostname()) doesn't really work here...
         tmp_sock = socket(AF_INET, SOCK_DGRAM)
         tmp_sock.connect(("8.8.8.8", 0))
-        print "local ip address is %s" % tmp_sock.getsockname()[0]
+        self.local_ipaddr = tmp_sock.getsockname()[0]
+        print "local ip address is %s\n" % self.local_ipaddr
         
         self.known_ip_addr_dict = {}
         
@@ -39,10 +40,10 @@ def main():
     
     while True:
         pkt = s.recvfrom(65535)
-        parse_pkt(pkt[0])
+        parse_pkt(watchDog, pkt[0])
 
 
-def parse_pkt(pkt_raw):
+def parse_pkt(watchDog, pkt_raw):
     ''' parses the packet.'''
     
     # Parsing the IP header...
@@ -52,10 +53,22 @@ def parse_pkt(pkt_raw):
     ip_version = ip_version_ihl >> 4 # ip version is got.
     if ip_version == 4:
         ip_src_addr_strVal = inet_ntoa(ip_header[8]) # ip source addr is got.
-        ip_dst_addr_strVal = inet_ntoa(ip_header[8]) # ip dest addr is got.
+        ip_dst_addr_strVal = inet_ntoa(ip_header[9]) # ip dest addr is got.
+        src_dname = ""
+        dst_dname = ""
+        try:
+            src_dname = gethostbyaddr(ip_src_addr_strVal)[0]
+        except:
+            src_dname = "cannot.get.such.host"
+        
+        if ip_dst_addr_strVal != watchDog.local_ipaddr:
+            print "This is not right... dest addr is not local"
+        else:
+            dst_dname = "localhost"
+            
         print "Got a packet, src = %s (%s), dst = %s (%s)" % \
-                (ip_src_addr_strVal, gethostbyaddr(ip_src_addr_strVal), \
-                ip_dst_addr_strVal, gethostbyaddr(ip_dst_addr_strVal))
+                (ip_src_addr_strVal, src_dname, \
+                ip_dst_addr_strVal, dst_dname)
         
     
     
